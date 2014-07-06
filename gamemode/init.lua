d56b8f3bd09a3ba0c8081a0b07cb060cdac90654
RadioactiveMonkey <christophermilner@btinternet.com>
2014-07-06 13:35:40 +0100

    Player Stuck Table and Hunter Damage Effects
    Adds a table that records whether a player is currently stuck or not.
    Also adds a feature meaning that when a hunter takes damage their screen
    is darkened and movement speed is reduced. This allows the hunted
    players to fight back slightly.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,9 +3,12 @@ AddCSLuaFile( "shared.lua" )
 
 include( 'shared.lua' )
 
+PlayerStuckTable = {}
+
 -- Allows user to pick teams
 function GM:PlayerInitialSpawn( ply )
-	ply:ConCommand( "team_menu" )	
+	ply:ConCommand( "team_menu" )
+	table.insert(PlayerStuckTable , ply:UserID() , false)
 end
 
 -- Assigns different weapons to players dependent on their team
@@ -13,10 +16,12 @@ function GM:PlayerLoadout( ply )
 
 	ply:StripWeapons()
 
+	ply:ChatPrint( "Player Loadout Function Called" )
+
 	if ply:Team() == 1 then
 		ply:Give( "weapon_pistol" )
 		ply:Give( "weapon_crowbar" )
-	else
+	elseif ply:Team() == 2 then
 		ply:Give( "weapon_ar2" )
 	end
 end
@@ -27,10 +32,12 @@ function GM:PlayerHurt(ply , atk)
 
 	if ply:Team() == 2 then
 		MakePlayerStuck(ply)
+	elseif ply:Team() == 1 then
+		DarkenScreen(ply)
 	end
 end
 
---Switches player to team 1
+-- Switches player to team 1
 function team_1(ply)
 	ply:SetTeam(1)
 	ply:Spawn()
@@ -38,7 +45,7 @@ function team_1(ply)
 end
 concommand.Add( "team_1" , team_1)
 
---Switches player to team 2
+-- Switches player to team 2
 function team_2(ply)
 	ply:SetTeam(2)
 	ply:Spawn()
@@ -46,14 +53,40 @@ function team_2(ply)
 end
 concommand.Add( "team_2" , team_2)
 
-function MakePlayerStuck(ply)
+-- Slows the player down massively
+function MakePlayerStuck( ply )
 	ply:SetWalkSpeed(20)
 	ply:SetRunSpeed(20)
+	PlayerStuckTable[ ply:UserID() ] = true
 end
 concommand.Add( "stick" , MakePlayerStuck)
 
-function UnstickPlayer(ply)
+-- Usticks  the player ressetting their speed to normal
+function UnstickPlayer( ply )
 	ply:SetWalkSpeed(250)
 	ply:SetRunSpeed(500)
+	PlayerStuckTable[ ply:UserID() ] = false
 end
 concommand.Add( "unstick" , UnstickPlayer)
+
+--Returns a boolean representative of whether the player is stuck
+function IsPlayerStuck( ply )
+	return PlayerStuckTable[ ply:UserID() ]
+end
+concommand.Add( "amistuck" , IsPlayerStuck)
+
+--Darkens the screen and slows speed when a hunter is hit
+function DarkenScreen( ply )
+	ply:ScreenFade(2 , Color(0,0,0,100) , 0.1 , 1.5)
+	ply:SetWalkSpeed(ply:GetWalkSpeed() - 20)
+	ply:SetRunSpeed(ply:GetRunSpeed() - 20)
+	timer.Create( "SpeedTimer" , 1 , (250 - ply:GetWalkSpeed()) / 5 , function() SpeedTimerCall(ply) end)
+end
+concommand.Add( "dark" , DarkenScreen )
+
+--Called to increase the run/walk speed
+function SpeedTimerCall( ply )
+	ply:SetWalkSpeed( ply:GetWalkSpeed() + 5)
+	ply:SetRunSpeed( ply:GetRunSpeed() + 5)
+	ply:ChatPrint(ply:GetWalkSpeed())
+end
